30c57c7c303d324331dd2f30692b89a7d5b336f1
Matheus Wassermann <matheus5wassermann@gmail.com>
2025-09-15 14:25:38 -0300

    Teste prático da Iniflex

---
+++ Industria/src/model/Funcionario.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa{
+
+    private BigDecimal salario;
+    private String funcao;
+
+    public Funcionario(LocalDate dataNascimento, String nome, BigDecimal salario, String funcao) {
+        super(dataNascimento, nome);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+}
